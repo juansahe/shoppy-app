a3a8364e09410fb3224ec5a259ab8b94022253d5
cristiansc0794 <cristian.sarmiento@grimorum.com>
2017-06-07 11:58:18 -0500

    registro arreglo ionic css

--- src/js/register/register.controller.js
+++ src/js/register/register.controller.js
@@ -1,5 +1,6 @@
 class RegisterCtrl{
 	constructor(RegisterService, $scope, $location){
+		
 		this.RegisterService = RegisterService;
 		$scope.user={
 			puntos:"100",
@@ -12,8 +13,44 @@ class RegisterCtrl{
 
 			 $location.path('/completeprofile');	
 		}
-
+		$scope.onezoneDatepicker = {
+			date: new Date(), // MANDATORY                     
+	        mondayFirst: false,                
+	        months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],                    
+	        daysOfTheWeek: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],     
+	        startDate: new Date(1950, 1, 26),             
+	        endDate: new Date(2024, 1, 26),                    
+	        disablePastDays: false,
+	        disableSwipe: false,
+	        disableWeekend: false,
+	        disableDates: [new Date(2016, 1, 15), new Date(2016, 2, 16), new Date(2016, 3, 17)],
+	        showDatepicker: false,
+	        showTodayButton: true,
+	        calendarMode: false,
+	        hideCancelButton: false,
+	        hideSetButton: false,
+	        highlights: [
+	                    {
+	                        date: new Date(2016, 1, 7),
+	                        color: '#FFF',
+	                        textColor: '#fff'
+	                    },
+	                    {
+	                        date: new Date(2016, 1, 18)
+	                    },
+	                    {
+	                        date: new Date(2016, 1, 19)
+	                    },
+	                    {
+	                        date: new Date(2016, 1, 20)
+	                    }
+	                ],
+	        callback: function(val){
+	            
+	        }
+	    }
 	}
+
 }
 
 angular.module('Grimorum.register').controller('RegisterCtrl', RegisterCtrl);
