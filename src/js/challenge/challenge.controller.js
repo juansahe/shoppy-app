d52016dd4392d15b3216fcf23f89907affb20c54
cristiansc0794 <cristian.sarmiento@grimorum.com>
2017-06-13 09:51:43 -0500

    menu

--- src/js/challenge/challenge.controller.js
+++
@@ -1,8 +0,0 @@
-class ChallengeCtrl{
-	constructor(ChallengeService, $scope){
-		this.ChallengeService = ChallengeService;
-
-		
-	}
-}
-angular.module('Grimorum.challenge').controller('ChallengeCtrl', ChallengeCtrl);
