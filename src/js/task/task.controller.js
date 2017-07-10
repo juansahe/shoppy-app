cbda70f28c8607b36b82ce084a1bb366ba18ddb2
Santiago Rodriguez <santiago.rodriguez@grimorum.com>
2017-07-10 12:23:38 -0500

    cmabios task

--- src/js/task/task.controller.js
+++ src/js/task/task.controller.js
@@ -1,29 +1,6 @@
 class TaskCtrl {
   constructor(TaskService, $scope, $cordovaCamera, $ionicPopup, $rootScope, $location, Session) {
 
-     document.addEventListener("deviceready", function () {
-
-    var options = {
-      quality: 50,
-      destinationType: Camera.DestinationType.DATA_URL,
-      sourceType: Camera.PictureSourceType.CAMERA,
-      allowEdit: true,
-      encodingType: Camera.EncodingType.JPEG,
-      targetWidth: 100,
-      targetHeight: 100,
-      popoverOptions: CameraPopoverOptions,
-      saveToPhotoAlbum: false,
-	  correctOrientation:true
-    };
-
-    $cordovaCamera.getPicture(options).then(function(imageData) {
-      var image = document.getElementById('myImage');
-      image.src = "data:image/jpeg;base64," + imageData;
-      console.log("cam")
-    }, function(err) {
-      // error
-    });
-
    // $scope.$digest();
 
     this.TaskService = TaskService;
