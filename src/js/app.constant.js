8df70acfb7d25eb1992ddc7eb9933652484c44ed
Juan D Sanchez <juansahe@gmail.com>
2017-07-25 08:56:19 -0500

    first commit

--- src/js/app.constant.js
+++ src/js/app.constant.js
@@ -1,7 +1,7 @@
 angular.module('Grimorum')
   .constant("CONFIG", {
-    "url": "http://45.55.43.26:8000/api/",
-    "API_URL": "http://45.55.43.26:8000/api/"
+    "url": "http://34.206.99.185:8000/api/",
+    "API_URL": "http://34.206.99.185:8000/api/"
     // "url": "http://192.168.0.24:8000/api/",
     // "API_URL" : "http://192.168.0.24:8000/api/"
 
